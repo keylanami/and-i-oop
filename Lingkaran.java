0062baf6d08a6ed76322f4b2a4154393233621a5
klyroe <keylaanmrr@gmail.com>
2025-11-04 20:22:01 +0700

    anotasi penjelasan latsol 1

--- Lingkaran.java
+++ Lingkaran.java
@@ -1,17 +1,16 @@
 public class Lingkaran {
+    private static double pi = 3.14; //this is where i initiated the pi number, could use Math.pi or other way but yeah im only being a bit experimental here
 
-    private static double pi = 3.14;
+    public static double luas(float r){ // same method name, different parameter
+        return r * r * pi; //just rumus
+    }
 
-    public static double luas(float r){
+    public static double luas(double d){ // it's like treating 2 people with the same name, a different treatment.
+        double r = d/2; // but in this case, i made a condition if the argument's data type is double, it will be considered not only as a double but a diameter.
         return r * r * pi;
     }
 
-    public static double luas(double d){
-        double r = d/2;
-        return r * r * pi;
-    }
-
-    public static double keliling(float r){
+    public static double keliling(float r){ // same goes to this. 
         return r * 2 * pi;
     }
 
@@ -19,3 +18,6 @@ public class Lingkaran {
         return d * pi;
     }
 }
+
+// overloading can still work on static
+// all i know is Java will choose either of the same method as it first compiles the code
--- Main01.java
+++ Main01.java
@@ -5,5 +5,10 @@ public class Main01 {
         System.out.println("Lingkaran dengan jejari 5.5\nLuas & Keliling\n" + Lingkaran.luas(5.5) + " | " + Lingkaran.keliling(5.5));
         System.out.println("Lingkaran dengan jejari 20.4\nLuas & Keliling\n" + Lingkaran.luas(20.4) + " | " + Lingkaran.keliling(20.4));
 
+        // argument yg dilempar ke parameter ini tipe datanya beda-beda as in ada yang angka satuan dan decimal.
+        // but however, nama methodnya sama. it's called overloading. Load. ngisi. 
+        // so ofc it's chosen at compile time. 
+        // how does it go? of course we are nawt gonna do an if-else here but we let Java itself handles by this so called overloading
+
     }
 }
